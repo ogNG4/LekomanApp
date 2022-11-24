7a724dc10b45bdbd6c6c1481c8830a1719168059
ogNGA <m.podobinski33@gmail.com>
2022-11-24 16:27:20 +0100

    Dodaj pliki projektów.

---
+++ LekomanApp/LekomanApp/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using LekomanApp.Tables;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LekomanApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDatabase.db");
+            var db = new SQLiteConnection(dbpath);
+            db.CreateTable<RegUserTable>();
+
+            var item = new RegUserTable()
+            {
+                UserName = EntryUserName.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text
+                
+            };
+
+            
+
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async () => {
+
+                var result = await this.DisplayAlert("Gratulacje", "Rejestracja przebiegła pomyślnie", "OK", "Anuluj");
+
+                if (result)
+                    await Navigation.PushAsync(new LoginPage());
+            });
+        }
+    }
+}
